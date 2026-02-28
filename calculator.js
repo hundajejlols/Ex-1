562035b444fc4143d6b651a2c7a16656939a4cc8
Hundajej <g2fdby2jh9@privaterelay.appleid.com>
2026-02-28 09:58:51 +0100

    zadanie

---
+++ calculator.js
@@ -0,0 +1,12 @@
+
+export function odejowanie(numberA, numberB) {
+    return numberA - numberB;
+}
+
+export function mnozenie(numberA, numberB) {
+    return numberA * numberB;
+}
+
+export function dzielenie(numberA, numberB) {
+    return numberA / numberB;
+}
---
+++ runner.js
@@ -0,0 +1,9 @@
+import { dzielenie, mnozenie, odejowanie } from "./calculator.js";
+
+const result1 = odejowanie(3,4);
+
+const result2 = mnozenie(3,4);
+
+const result3 = dzielenie(3,4);
+
+console.log(result1, result2, result3)
